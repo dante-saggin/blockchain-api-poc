91c4f138e61fafbf16d97d7c8c972827f87b3130
Saggin <dante.saggin@accenture.com>
2018-10-09 18:39:20 -0300

    Unit Test Proof of Work and Valid Proof

--- testBlockChainMethods.py
+++ testBlockChainMethods.py
@@ -10,6 +10,15 @@ class TestBlockChainMethods(unittest.TestCase):
         self.assertEqual(block["proof"], 100)
         self.assertEqual(block["previous_hash"], 123)
 
+    def test_validate_prof(self):
+        self.assertTrue(self.chain.valid_proof(1, 72608))
+        self.assertFalse(self.chain.valid_proof(1, 2))
+
+    def test_proof_of_work(self):
+        self.assertEqual(self.chain.proof_of_work(1), 72608)
+        self.assertEqual(self.chain.proof_of_work(2), 69926)
+        self.assertNotEquals(self.chain.proof_of_work(2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
